a5917e8e59cffd7c8200857c9f35dd487e403f98
tempest <tempest0580@gmail.com>
2019-06-08 23:38:35 -0400

    Update Temptv to 1.0.3e

--- plugin.video.temptv/default.py
+++ plugin.video.temptv/default.py
@@ -35,7 +35,7 @@ def MAIN():
     addDir('27/7', userlists.hour24, 2, 'https://depo8.com/wp-content/uploads/2014/07/24-7-icon.png')
     addDir('Pluto TV', userlists.pluto, 2, 'https://images-na.ssl-images-amazon.com/images/I/519H2haYtzL._SY355_.png')
     addDir('1 Click Movies', '', 7, 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/c72e2f68-1a1c-48a2-aa5f-edf7eaaf3548/d704h5l-8e40963d-18aa-4982-a412-5c0ef52f6acf.png')
-    #addDir('1 Click Shows', '', 11, 'http://chittagongit.com/images/tv-series-icon/tv-series-icon-15.jpg')
+    addDir('1 Click Shows', '', 11, 'http://chittagongit.com/images/tv-series-icon/tv-series-icon-15.jpg')
     #addDir('On Demand Movies', '', 12, AddonIcon)
     #addDir('On Demand Shows', '', 13, AddonIcon)
     addDir('UK', userlists.uk, 2, 'https://maxcdn.icons8.com/app/uploads/2016/11/refresh1.png')
@@ -58,7 +58,7 @@ def VOD_SHOWS():
 
 def CLICK_SHOWS():
     addDir('Smallville', '', 14, 'https://m.media-amazon.com/images/M/MV5BYmNiNzlhOWItMDM5Mi00MGYzLWI1ZDYtNmI5NzI0MWFkMTIwXkEyXkFqcGdeQXVyNjU2NjA5NjM@._V1_.jpg')
-    addDir('Anger Managment', userlists.angermanagment, 2, 'https://i1.wp.com/hitenism.com/wp-content/uploads/Fotolia_61714948_L.jpg?ssl=1')
+    #addDir('Anger Managment', userlists.angermanagment, 2, 'https://i1.wp.com/hitenism.com/wp-content/uploads/Fotolia_61714948_L.jpg?ssl=1')
     addDir('Family Guy', '', 15, 'https://m.media-amazon.com/images/M/MV5BODEwZjEzMjAtNjQxMy00Yjc4LWFlMDAtYjhjZTAxNDU3OTg3XkEyXkFqcGdeQXVyOTM2NTM4MjA@._V1_.jpg')
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
--- plugin.video.temptv/userlists.py
+++ plugin.video.temptv/userlists.py
@@ -36,6 +36,7 @@ smallville2 = 'https://raw.githubusercontent.com/Tempest0580/Temptv/master/lists
 smallville3 = 'https://raw.githubusercontent.com/Tempest0580/Temptv/master/lists/shows/smallville3.txt'
 angermanagment = 'https://raw.githubusercontent.com/Tempest0580/Temptv/master/lists/shows/angermanagment.txt'
 familyguy1 = 'https://raw.githubusercontent.com/Tempest0580/Temptv/master/lists/shows/familyguy1.txt'
+familyguy2 = 'https://raw.githubusercontent.com/Tempest0580/Temptv/master/lists/shows/familyguy2.txt'
 familyguy12 = 'https://raw.githubusercontent.com/Tempest0580/Temptv/master/lists/shows/familyguy12.txt'
 familyguy13 = 'https://raw.githubusercontent.com/Tempest0580/Temptv/master/lists/shows/familyguy13.txt'
 
