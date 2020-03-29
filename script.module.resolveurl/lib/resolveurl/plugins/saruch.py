48f6a7608fda52fcde885cfa20d8741a15bd47a4
Tempest <tempest0580@gmail.com>
2020-03-29 13:10:35 -0400

    Read Changelog

--- script.module.resolveurl/lib/resolveurl/plugins/saruch.py
+++ script.module.resolveurl/lib/resolveurl/plugins/saruch.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 """
-    Kodi resolveurl plugin
-    Copyright (C) 2016  alifrezser
+    Plugin fore ResolveURL
+    Copyright (C) 2020  gujal
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -15,16 +15,16 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import re
+import json
 from lib import helpers
 from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class VideaResolver(ResolveUrl):
-    name = "videa"
-    domains = ["videa.hu", "videakid.hu"]
-    pattern = r'(?://|\.)((?:videa|videakid)\.hu)/(?:player/?\?v=|player/v/|videok/)(?:.*-|)([0-9a-zA-Z]+)'
+class SaruchResolver(ResolveUrl):
+    name = "saruch"
+    domains = ["saruch.co"]
+    pattern = r'(?://|\.)(saruch\.co)/(?:embed|video)/([0-9a-zA-Z]+)'
 
     def __init__(self):
         self.net = common.Net()
@@ -32,19 +32,21 @@ class VideaResolver(ResolveUrl):
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
         headers = {'User-Agent': common.FF_USER_AGENT,
-                   'Referer': 'https://{0}/player?v={1}'.format(host, media_id),
+                   'Referer': 'https://{0}/videos/{1}'.format(host, media_id),
                    'X-Requested-With': 'XMLHttpRequest'}
-        html = self.net.http_GET(web_url, headers=headers).content
-        sources = re.findall(r'video_source\s*name="(?P<label>[^"]+)[^>]+>(?P<url>[^<]+)', html)
+        js_data = json.loads(self.net.http_GET(web_url, headers=headers).content)
 
-        if sources:
+        if js_data.get('video').get('sources'):
+            sources = []
+            for item in js_data.get('video').get('sources'):
+                sources.append((item.get('label'), item.get('file')))
             source = helpers.pick_source(helpers.sort_sources_list(sources))
-            source = 'https:' + source if source.startswith('//') else source
             headers.pop('X-Requested-With')
-            headers.update({'Origin': 'https://{}'.format(host)})
-            return source.replace('&amp;', '&') + helpers.append_headers(headers)
+            de = js_data.get('de')
+            en = js_data.get('en')
+            return '{0}&de={1}&en={2}{3}'.format(source, de, en, helpers.append_headers(headers))
 
         raise ResolverError('Stream not found')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, 'https://{host}/videaplayer_get_xml.php?v={media_id}&start=0&referrer=http://{host}')
+        return self._default_get_url(host, media_id, 'https://api.saruch.co/videos/{media_id}/stream?referrer=https:%2F%2F{host}%2Fvideo%2F{media_id}')
--- script.module.resolveurl/lib/resolveurl/plugins/sibnet.py
+++ script.module.resolveurl/lib/resolveurl/plugins/sibnet.py
@@ -1,6 +1,6 @@
 '''
-    resolveurl XBMC Addon
-    Copyright (C) 2016 Gujal
+    Plugin for ResolveURL
+    Copyright (C) 2020 Gujal
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -21,10 +21,10 @@ from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class GamoVideoResolver(ResolveUrl):
-    name = 'GamoVideo'
-    domains = ['gamovideo.com']
-    pattern = r'(?://|\.)(gamovideo\.com)/(?:embed-)?([0-9a-zA-Z]+)'
+class SibnetResolver(ResolveUrl):
+    name = 'sibnet'
+    domains = ['sibnet.ru']
+    pattern = r'(?://|\.)(sibnet\.ru)/(?:shell\.php\?videoid=|.*video)([0-9a-zA-Z]+)'
 
     def __init__(self):
         self.net = common.Net()
@@ -32,21 +32,13 @@ class GamoVideoResolver(ResolveUrl):
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
         headers = {'User-Agent': common.RAND_UA,
-                   'Referer': 'http://gamovideo.com/player61/jwplayer.flash.swf'}
-        r = self.net.http_GET(web_url, headers=headers)
-        cookie = ''
-        for item in r.get_headers(as_dict=True)['Set-Cookie'].split('GMT,'):
-            cookie += item.split('path')[0]
-        headers.update({'Cookie': cookie + 'sugamun=1; invn=1; pfm=1'})
-
+                   'Referer': 'https://video.sibnet.ru/'}
         html = self.net.http_GET(web_url, headers=headers).content
-        html += helpers.get_packed_data(html)
-        source = re.search(r'''file:\s*["'](?P<url>http[^"']+)["']''', html)
-        headers.pop('Cookie')
+        source = re.search(r'src:\s*"([^"]+)', html)
         if source:
-            return source.group(1) + helpers.append_headers(headers)
+            return 'https://video.sibnet.ru' + source.group(1) + helpers.append_headers(headers)
 
         raise ResolverError('Video not found')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='http://{host}/embed-{media_id}.html')
+        return self._default_get_url(host, media_id, template='https://video.{host}/shell.php?videoid={media_id}')
